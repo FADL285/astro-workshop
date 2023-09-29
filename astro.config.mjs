d44348a458dcffe8b7f73f6fb729816278f29942
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-09-29 17:21:56 +0300

    add Configuration to upload to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-vue-shop.netlify.app/',
   output: 'hybrid',
-  integrations: [vue()]
+  integrations: [vue()],
+  adapter: netlify()
 });
